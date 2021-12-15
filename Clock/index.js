d0f1f4528a043f06dab05c14f69fe4878cee7e89
singlarobin <singlarobin99@gmail.com>
2021-12-15 22:17:29 +0530

    project Type Ahead created

--- Clock/index.js
+++ Clock/index.js
@@ -6,16 +6,29 @@ const setDate = () => {
     const now = new Date();
 
     const hour = now.getHours();
-    const hoursDegree = ((hour/12)* 360) + 90;
+    const hoursDegree = ((hour / 12) * 360) + 90;
     hourHand.style.transform = `rotate(${hoursDegree}deg)`;
 
     const minute = now.getMinutes();
-    const minutesDegree = ((minute/60)* 360) + 90;
+    const minutesDegree = ((minute / 60) * 360) + 90;
     minuteHand.style.transform = `rotate(${minutesDegree}deg)`;
 
     const seconds = now.getSeconds();
     const secondsDegree = ((seconds / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+    // secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+
+    if(seconds == 0){
+        secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+        // secondHand.style.transition = null;
+    }
+    else{
+        secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+        // secondHand.style.transition = 'all 0.05s cubic-bezier(0.1, 2.7, 0.58, 1)';
+    }
+
+    console.log('hour:', hour, '    ', hoursDegree);
+    console.log('min:', minute, '    ', minutesDegree);
+    console.log('sec:', seconds, '   ', secondsDegree);
 };
 
 setInterval(setDate, 1000);
